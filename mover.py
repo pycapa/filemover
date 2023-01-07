4459244bbbcbc19eabe229fb417c6653e600dfaf
pycapa <bycapus@gmail.com>
2023-01-06 19:19:21 -0500

    audio and others folders

--- mover.py
+++ mover.py
@@ -16,5 +16,49 @@ pictureFolder = params['picturesFolder']
 audioFolder = params['audioFolder']
 compressedFolder = params['compressedFolder']
 othersFolder = params['others']
+videoFolder = params['videoFolder']
+extensions = params['filesExtensions']
 
-print(audioFolder)
+if __name__ == "__main__":
+
+
+    counter = 0
+    for filename in os.listdir(downloadFolder):
+        name, extension = os.path.splitext(downloadFolder + filename)
+
+        # validating folder
+        if os.path.exists(pictureFolder) == False: os.mkdir(pictureFolder)
+
+        if os.path.exists(audioFolder) == False: os.mkdir(audioFolder)
+
+        if os.path.exists(videoFolder) == False: os.mkdir(videoFolder)
+
+        if os.path.exists(compressedFolder) == False: os.mkdir(compressedFolder)
+
+        if os.path.exists(othersFolder) == False: os.mkdir(othersFolder)
+        
+        if  extension in extensions['picturesExtensions']: # compress image file and move them to picture folder.
+            picture = Image.open(downloadFolder + filename)
+            picture.save(pictureFolder + "compressed_" + filename, optimize=True)
+            os.remove(downloadFolder + filename)
+            counter += 1
+        elif extension in extensions['audioExtensions']: # compress image file and move them to picture folder.
+                os.rename(downloadFolder + filename, audioFolder + filename)
+                counter += 1
+        elif extension in extensions['videoExtensions']: # compress image file and move them to picture folder.
+            os.rename(downloadFolder + filename, videoFolder + filename)
+            counter += 1
+        elif extension in extensions['compressedExtensions']: # rename compressed files to compressed folderr.
+            os.rename(downloadFolder + filename, compressedFolder + filename)
+            counter += 1
+        else:
+            os.rename(downloadFolder + filename, othersFolder + filename)
+            counter += 1
+
+
+
+    print(f'there were compressed and moved {counter} file(s)')
+
+
+        
+
